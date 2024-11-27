a94970346470559bfe2857d14b0a4c1b69e6ffe3
geger24 <vincent-gervais@orange.fr>
2024-11-27 18:00:01 +0100

    Ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,10 @@
 import Route from "./Route.js";
 
 //Définir ici vos routes
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
